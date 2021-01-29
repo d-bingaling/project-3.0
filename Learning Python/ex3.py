c3548a38186ae1b4d295fac33e5672670adf2f17
d-bingaling <dan.bingley@wegotpop.com>
2021-01-29 17:37:15 +0000

    python lessons 1 to 5

---
+++ Learning Python/ex3.py
@@ -0,0 +1,40 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percentage
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+
+# print("I will now count my chickens:")
+
+# print("Hens", 25 + 30 / 6)
+# print("Roosters", 100 - 25 * 3 % 4)
+
+# print("Now I will count my eggs:")
+
+# print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# print("Is it true that 3 + 2 < 5 - 7?")
+
+# print(3 + 2 < 5 - 7)
+
+# print("What is 3 + 2?", 3 + 2)
+# print("What is 5 - 7?", 5 - 7)
+
+# print("Oh, that's why it's False.")
+
+# print("How about some more.")
+
+# print("Is it greater?", 5 > -2)
+# print("Is it greater or equal?", 5 >= -2)
+# print("Is it less or equal?", 5 <= -2)
+
+
+
+print("How much will my shopping come to?")
+
+print("£", (3.00 + 12.5 + 9.00 - 10.00 / 3.00))
