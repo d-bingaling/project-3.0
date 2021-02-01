63e2aea0215704764442bed914187158ba46d1bf
Daniella Bingley <50371295+d-bingaling@users.noreply.github.com>
2021-02-01 10:50:32 +0000

    Revert "session 1"

--- Learning Python/ex1.py
+++
@@ -1,9 +0,0 @@
-print("Hello World!")
-print("Hello Again")
-print("I like typing this.")
-print("This is fun.")
-print('Yay! Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
-
-# print("yesssss look at all this stuff i'm printing") i guess adding a hash comments it out
