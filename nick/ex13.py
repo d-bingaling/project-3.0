5c576b582fa635b98932eff573d066c977c7b6b0
Nick Cresner <nickcresner@Admins-MacBook-Pro.local>
2021-07-06 17:05:49 +0100

    Python exercise 14

---
+++ nick/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# red the WYSS section for how to run this
+zero, first, second, third = argv
+
+print("The script is called:", zero)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
